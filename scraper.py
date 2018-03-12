d206a0818536ddeb3c20bead96360b347f781d97
blablupcom <o.volodin@yahoo.com>
2018-03-12 16:05:47 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -11,7 +11,10 @@ from bs4 import BeautifulSoup
 
 #### FUNCTIONS 1.1
 import ssl
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 import requests
+
 
 def validateFilename(filename):
     filenameregex = '^[a-zA-Z0-9]+_[a-zA-Z0-9]+_[a-zA-Z0-9]+_[0-9][0-9][0-9][0-9]_[0-9QY][0-9]$'
@@ -39,22 +42,19 @@ def validateFilename(filename):
 
 def validateURL(url):
     try:
-        req = urllib2.Request(url.encode("utf-8"))
-        gcontext = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
-        r = urllib2.urlopen(req, context=gcontext)
+
+        r = requests.get(url, verify=False)
         count = 1
-        while r.getcode() == 500 and count < 4:
+        while r.status_code == 500 and count < 4:
             print ("Attempt {0} - Status code: {1}. Retrying.".format(count, r.status_code))
             count += 1
-            req = urllib2.Request(url.encode("utf-8"))
-            gcontext = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
-            r = urllib2.urlopen(req, context=gcontext)
+            r = requests.get(url, verify=False)
         sourceFilename = r.headers.get('Content-Disposition')
         if sourceFilename:
             ext = os.path.splitext(sourceFilename)[1].replace('"', '').replace(';', '').replace(' ', '')
         else:
             ext = os.path.splitext(url)[1]
-        validURL = r.getcode() == 200
+        validURL = r.status_code == 200
         validFiletype = ext.lower() in ['.csv', '.xls', '.xlsx', '.pdf', '.xlt', '.zip']
         return validURL, validFiletype
     except:
@@ -71,7 +71,7 @@ def validate(filename, file_url):
         return False
     if not validURL:
         print filename, "*Error: Invalid URL*"
-        print file_url.encode("utf-8")
+        print file_url
         return False
     if not validFiletype:
         print filename, "*Error: Invalid filetype*"
